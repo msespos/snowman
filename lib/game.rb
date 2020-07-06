44d9f4372d24615b76454f6d0af80912d1cc780d
Mike Esposito <mikeesposito6852@gmail.com>
2020-07-06 13:39:53 -0700

    Reorder methods in Game

--- lib/game.rb
+++ lib/game.rb
@@ -11,11 +11,34 @@ class Game
     @incorrect_guesses = []
   end
 
-  def new_secret_word
-    word_list = File.readlines "5desk.txt"
-    word_list.map! { |word| word[0..-3] }
-    word_list.delete_if { |word| word != word.downcase || word.length < 5 || word.length > 12 }
-    @secret_word = word_list.sample
+  def save_text
+    puts "There are 3 slots to save in, labeled 1, 2, and 3."
+    puts "Please select a number. You will overwrite any already saved game in that slot."
+  end
+
+  def slot
+    slot = gets.chomp
+    until slot == "1" || slot == "2" || slot == "3"
+      puts "Please select a number between 1 and 3."
+      slot = gets.chomp
+    end
+    slot
+  end
+
+  def to_yaml
+    YAML.dump ({
+      :secret_word => @secret_word,
+      :unguessed_letters => @unguessed_letters,
+      :correct_guesses => @correct_guesses,
+      :incorrect_guesses => @incorrect_guesses,
+      :board => @board
+    })
+  end
+
+  def save_game
+    save_text
+    File.open("saved_games/snowman_save_#{slot}.yml", "w").puts(to_yaml)
+    puts "Game saved! Let's get back to the game."
   end
 
   def guessed_already?(guess)
@@ -46,60 +69,6 @@ class Game
     @unguessed_letters.delete(guess)
   end
 
-  def to_yaml
-    YAML.dump ({
-      :secret_word => @secret_word,
-      :unguessed_letters => @unguessed_letters,
-      :correct_guesses => @correct_guesses,
-      :incorrect_guesses => @incorrect_guesses,
-      :board => @board
-    })
-  end
-
-  def from_yaml(game)
-    status = YAML.load(File.read(game))
-    @secret_word = status[:secret_word]
-    @unguessed_letters = status[:unguessed_letters]
-    @correct_guesses = status[:correct_guesses]
-    @incorrect_guesses = status[:incorrect_guesses]
-    @board = status[:board]
-  end
-
-  def save_text
-    puts "There are 3 slots to save in, labeled 1, 2, and 3."
-    puts "Please select a number. You will overwrite any already saved game in that slot."
-  end
-
-  def load_text
-    puts "There are 3 slots that a game could be saved in, labeled 1, 2, and 3."
-    puts "Please select a number. You should have saved a game in that slot already."
-  end
-
-  def slot
-    slot = gets.chomp
-    until slot == "1" || slot == "2" || slot == "3"
-      puts "Please select a number between 1 and 3."
-      slot = gets.chomp
-    end
-    slot
-  end
-
-  def load_game?
-    puts "Would you like to load a saved game?\nEnter y for yes, any other key for no"
-    gets.chomp.downcase == "y"
-  end
-
-  def save_game
-    save_text
-    File.open("saved_games/snowman_save_#{slot}.yml", "w").puts(to_yaml)
-    puts "Game saved! Let's get back to the game."
-  end
-
-  def load_game
-    load_text
-    from_yaml("saved_games/snowman_save_#{slot}.yml")
-  end
-
   def finish
     if @board.word_solved?
       puts "Congratulations! You win!\n"
@@ -128,6 +97,37 @@ class Game
       Good luck!
 
     HEREDOC
+  end
+
+  def load_game?
+    puts "Would you like to load a saved game?\nEnter y for yes, any other key for no"
+    gets.chomp.downcase == "y"
+  end
+
+  def load_text
+    puts "There are 3 slots that a game could be saved in, labeled 1, 2, and 3."
+    puts "Please select a number. You should have saved a game in that slot already."
+  end
+
+  def from_yaml(game)
+    status = YAML.load(File.read(game))
+    @secret_word = status[:secret_word]
+    @unguessed_letters = status[:unguessed_letters]
+    @correct_guesses = status[:correct_guesses]
+    @incorrect_guesses = status[:incorrect_guesses]
+    @board = status[:board]
+  end
+
+  def load_game
+    load_text
+    from_yaml("saved_games/snowman_save_#{slot}.yml")
+  end
+
+  def new_secret_word
+    word_list = File.readlines "5desk.txt"
+    word_list.map! { |word| word[0..-3] }
+    word_list.delete_if { |word| word != word.downcase || word.length < 5 || word.length > 12 }
+    @secret_word = word_list.sample
   end
 
   def display_dashes
